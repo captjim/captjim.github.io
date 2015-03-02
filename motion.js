7f3f75608012ca7227c3092e550b3d5d698d6e00
Tom Rogers <tom@boomf.com>
2015-03-02 20:04:52 +0000

    close

--- motion.js
+++ motion.js
@@ -3,14 +3,13 @@ var counter = 0;
 var timer = 10;
 
 window.addEventListener("devicemotion", function(event) {
-  if (event.acceleration.x > 10) {
+  if (event.acceleration.x > 5) {
     counter = counter + 1;
     setTimeout(function(){
       if (counter < 3) {
         counter = 0;
       }
     }, 3000);
-    alert(counter);
   }
   if (counter > 2) {
     var n = Math.floor(3*Math.random());
