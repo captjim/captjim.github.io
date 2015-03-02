7758b5974356642f3e58add5d7a2a845b503b312
Tom Rogers <tom@boomf.com>
2015-03-02 19:47:53 +0000

    back again

--- motion.js
+++ motion.js
@@ -2,16 +2,16 @@ var counter = 0;
 var timer = 10;
 
 window.addEventListener("devicemotion", function(event) {
-  var qualifies = (event.acceleration.x > 10) && (event.interval < 1000 || counter == 0);
+  var qualifies = (event.acceleration.x > 10);
   if (qualifies) {
     counter +=;
-  }
-  else {
-    counter = 0;
+    setTimeout(function(){
+      if (counter < 3) {
+        counter = 0;
+      }
+    }, 3000);
   }
   if (counter > 2) {
     alert(event.interval);
   }
-  // Process event.acceleration, event.accelerationIncludingGravity,
-  // event.rotationRate and event.interval
 }, true);
