28b42c671b235c43b02f7f06fd5d32ceb2cd4845
Tom Rogers <tom@boomf.com>
2015-03-02 19:31:51 +0000

    test

--- motion.js
+++ motion.js
@@ -1,6 +1,17 @@
+var counter = 0;
+var timer = 10;
+
 window.addEventListener("devicemotion", function(event) {
   if (event.acceleration.x > 10) {
-    alert("accel");
+    counter +=
+    setTimeout(function(){
+      if (counter < 3) {
+        counter = 0;
+      }
+    }, timer*1000)
+  }
+  if (counter > 2) {
+    alert(event.interval);
   }
   // Process event.acceleration, event.accelerationIncludingGravity,
   // event.rotationRate and event.interval
