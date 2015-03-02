645771edfe399b068fbe93a81f9df6dce49ef75c
Tom Rogers <tom@boomf.com>
2015-03-02 19:18:46 +0000

    10

--- motion.js
+++ motion.js
@@ -1,5 +1,5 @@
 window.addEventListener("devicemotion", function(event) {
-  if (event.acceleration.x > 0) {
+  if (event.acceleration.x > 10) {
     alert("accel");
   }
   // Process event.acceleration, event.accelerationIncludingGravity,
