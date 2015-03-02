125578f5efa8d7632f6888de915e7bd2b2968e32
Tom Rogers <tom@boomf.com>
2015-03-02 20:19:50 +0000

    dsfsdg

--- motion.js
+++ motion.js
@@ -1,15 +1,20 @@
 var array = ['Rock', 'Paper', 'Scissors'];
 var counter = 0;
 var timer = 10;
+var pasuse = false;
 
 window.addEventListener("devicemotion", function(event) {
-  if ((event.acceleration.y > 7) || (event.acceleration.x > 7)) {
+  if ((event.acceleration.y > 7) || (event.acceleration.x > 7) && !pause) {
     counter = counter + 1;
+    pause = true;
+    setTimeout(function(){
+      pause = false;
+    }, 1000)
     setTimeout(function(){
       if (counter < 3) {
         counter = 0;
       }
-    }, 3000);
+    }, 4000);
   }
   if (counter > 2) {
     var n = Math.floor(3*Math.random());
