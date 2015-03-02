f139b98bf7b2a4ccae61fb9c7c1a775465a09ee1
Tom Rogers <tom@boomf.com>
2015-03-02 20:15:59 +0000

    sgdgdsfg

--- motion.js
+++ motion.js
@@ -3,7 +3,7 @@ var counter = 0;
 var timer = 10;
 
 window.addEventListener("devicemotion", function(event) {
-  if ((event.acceleration.y > 4) || (event.acceleration.x > 4)) {
+  if ((event.acceleration.y > 7) || (event.acceleration.x > 7)) {
     counter = counter + 1;
     setTimeout(function(){
       if (counter < 3) {
