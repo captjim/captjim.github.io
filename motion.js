14cda64e1f0b0c2b7406df537e1a0b6f0620c638
Tom Rogers <tom@boomf.com>
2015-03-02 20:01:18 +0000

    semit

--- motion.js
+++ motion.js
@@ -4,12 +4,12 @@ var timer = 10;
 
 window.addEventListener("devicemotion", function(event) {
   if (event.acceleration.x > 1) {
-    counter +=;
-    // setTimeout(function(){
-    //   if (counter < 3) {
-    //     counter = 0;
-    //   }
-    // }, 3000);
+    counter = counter + 1;
+    setTimeout(function(){
+      if (counter < 3) {
+        counter = 0;
+      }
+    }, 3000);
     alert(counter);
   }
   if (counter > 2) {
