dead9e1b93a186814d801885718f05b797c3eb3a
Tom Rogers <tom@boomf.com>
2015-04-29 22:22:02 +0100

    Better

--- paralell-traverse.js
+++ paralell-traverse.js
@@ -46,7 +46,9 @@ var argCreator = function(arg) {
 }
 
 var run = function() {
-  event.preventDefault();
+  if (event) {event.preventDefault();}
+  resultKey = [];
+  resultValue = [];
   document.getElementById('result').innerHTML = "";
   argCreator(document.getElementById('arg').value);
   new Parallel(getKeys(treeFn())).require({fn: treeFn, name: 'treeFn'}, {fn: argFn, name: 'argFn'}).map(traverseValue).then(logValue);
@@ -90,26 +92,27 @@ function traverseKey(object) {
 }
 
 function logKey() {
+  if (!arguments[0][0]) {return;}
   for (j = 0; j < arguments[0].length; j++) {
     if (arguments[0][j]) {
       resultKey.push(arguments[0][j]);
     }
   }
   var node1 = document.createElement("LI");
-  var node11 = document.createTextNode('Key result stack: ' + resultKey[0]);
+  var node11 = document.createTextNode('Key result stack: [' + resultKey[0] + ']');
   node1.appendChild(node11);
   document.getElementById('result').appendChild(node1);
   obj = treeFn();
-  for (k = resultKey[0].length - 1; k > 0; k--) {
+  for (k = resultKey[0].length - 1; k > 1; k--) {
     var node = document.createElement("LI");
-    var nodep = document.createTextNode('Key result stack: ' + Object.keys(obj));
+    var nodep = document.createTextNode(Object.keys(obj));
     node.appendChild(nodep);
     document.getElementById('result').appendChild(node);
     var inter = resultKey[0][k]
     obj = obj[inter];
   }
   var node2 = document.createElement("LI");
-  var node21 = document.createTextNode('Key result: ' + Object.keys(obj));
+  var node21 = document.createTextNode('Key result: ' + JSON.stringify(resultKey[0][0], null, 9));
   node2.appendChild(node21);
   document.getElementById('result').appendChild(node2);
 }
@@ -136,26 +139,27 @@ function traverseValue(object) {
 }
 
 function logValue() {
+  if (!arguments[0][0]) {return;}
   for (j = 0; j < arguments[0].length; j++) {
     if (arguments[0][j]) {
       resultValue.push(arguments[0][j]);
     }
   }
   var node1 = document.createElement("LI");
-  var node11 = document.createTextNode('Value result stack: ' + resultValue[0]);
+  var node11 = document.createTextNode('Value result stack: [' + resultValue[0] + ']');
   node1.appendChild(node11);
   document.getElementById('result').appendChild(node1);
   obj = treeFn();
   for (k = resultValue[0].length - 1; k > 0; k--) {
     var node = document.createElement("LI");
-    var nodep = document.createTextNode('Value result stack: ' + Object.keys(obj));
+    var nodep = document.createTextNode(Object.keys(obj));
     node.appendChild(nodep);
     document.getElementById('result').appendChild(node);
     var inter = resultValue[0][k]
     obj = obj[inter];
   }
   var node2 = document.createElement("LI");
-  var node21 = document.createTextNode('Value result: ' + obj);
+  var node21 = document.createTextNode('Value result: ' + JSON.stringify(obj,null, 5));
   node2.appendChild(node21);
   document.getElementById('result').appendChild(node2);
 }
