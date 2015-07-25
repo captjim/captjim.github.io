ef5152ed45d24c1cf7168caebb84f5ee70e7543a
Tom Rogers <tom@boomf.com>
2015-07-25 10:45:51 +0100

    makes random work

--- boris.js
+++ boris.js
@@ -71,6 +71,7 @@ function geoFindMe(xml, type, number){
       }
 
       if (type == "random") {
+        for (i=0; i<length; i++){ array.push(id) }
         id = array[Math.floor(Math.random()*array.length)];
         lat  = parseFloat(stations[id].getElementsByTagName('lat')[0].childNodes[0].textContent);
         long = parseFloat(stations[id].getElementsByTagName('long')[0].childNodes[0].textContent);
